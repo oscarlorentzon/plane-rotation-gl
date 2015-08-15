a13c262dd74b413de5bb7c003d29e61b4e671195
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2015-08-15 21:02:13 +0200

    Improve comments.

--- javascript/buffer.js
+++ javascript/buffer.js
@@ -8,22 +8,22 @@ function createBuffer(context) {
     var gl = context.gl,
         program = context.program;
 
-    // look up where the vertex data needs to go.
+    // Retrieve atrribute locations.
     var positionLocation = gl.getAttribLocation(program, "a_position");
     var colorLocation = gl.getAttribLocation(program, "a_color");
 
-    // lookup uniforms
+    // Retrieve uniform locations.
     var modelViewMatrixLocation = gl.getUniformLocation(program, "u_modelViewMatrix");
     var projectionMatrixLocation = gl.getUniformLocation(program, "u_projectionMatrix");
 
-    // Create a buffer for vertices.
+    // Create a buffer for vertices and assign tile positions.
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.enableVertexAttribArray(positionLocation);
     gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tileData.positions), gl.STATIC_DRAW);
 
-    // Create a buffer for colors.
+    // Create a buffer for colors and assign tile colors.
     var colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.enableVertexAttribArray(colorLocation);
